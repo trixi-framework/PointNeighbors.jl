3b6d15e34299b36609c8e1af916c2a64149a5ab8
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-05-16 16:45:42 +0200

    Export `NeighborListsNeighborhoodSearch`

--- src/PointNeighbors.jl
+++ src/PointNeighbors.jl
@@ -13,7 +13,7 @@ include("nhs_grid.jl")
 include("nhs_neighbor_lists.jl")
 
 export for_particle_neighbor
-export TrivialNeighborhoodSearch, GridNeighborhoodSearch
+export TrivialNeighborhoodSearch, GridNeighborhoodSearch, NeighborListsNeighborhoodSearch
 export initialize!, update!, initialize_grid!, update_grid!
 
 end # module PointNeighbors
