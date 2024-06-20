28f5ddc84dfc6aa99fee8be9b6548c9ab627dac9
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-20 14:08:00 +0200

    Add TLSPH benchmark

--- benchmarks/smoothed_particle_hydrodynamics.jl
+++ benchmarks/smoothed_particle_hydrodynamics.jl
@@ -4,19 +4,25 @@ using BenchmarkTools
 
 const TrivialNeighborhoodSearch = PointNeighbors.TrivialNeighborhoodSearch
 const GridNeighborhoodSearch = PointNeighbors.GridNeighborhoodSearch
+const PrecomputedNeighborhoodSearch = PointNeighbors.PrecomputedNeighborhoodSearch
 
 """
     benchmark_wcsph(neighborhood_search, coordinates; parallel = true)
 
 A benchmark of the right-hand side of a full real-life Weakly Compressible
 Smoothed Particle Hydrodynamics (WCSPH) simulation with TrixiParticles.jl.
+This method is used to simulate an incompressible fluid.
 """
 function benchmark_wcsph(neighborhood_search, coordinates; parallel = true)
     density = 1000.0
     fluid = InitialCondition(; coordinates, density, mass = 0.1)
 
     # Compact support == smoothing length for the Wendland kernel
-    smoothing_length = neighborhood_search.search_radius
+    if neighborhood_search isa PrecomputedNeighborhoodSearch
+        smoothing_length = neighborhood_search.neighborhood_search.search_radius
+    else
+        smoothing_length = neighborhood_search.search_radius
+    end
     smoothing_kernel = WendlandC2Kernel{ndims(neighborhood_search)}()
 
     sound_speed = 10.0
@@ -38,4 +44,34 @@ function benchmark_wcsph(neighborhood_search, coordinates; parallel = true)
 
     return @belapsed TrixiParticles.interact!($dv, $v, $u, $v, $u, $neighborhood_search,
                                               $fluid_system, $fluid_system)
+end
+
+"""
+    benchmark_tlsph(neighborhood_search, coordinates; parallel = true)
+
+A benchmark of the right-hand side of a full real-life Total Lagrangian
+Smoothed Particle Hydrodynamics (TLSPH) simulation with TrixiParticles.jl.
+This method is used to simulate an elastic structure.
+"""
+function benchmark_tlsph(neighborhood_search, coordinates; parallel = true)
+    material = (density = 1000.0, E = 1.4e6, nu = 0.4)
+    solid = InitialCondition(; coordinates, density = material.density, mass = 0.1)
+
+    # Compact support == smoothing length for the Wendland kernel
+    if neighborhood_search isa PrecomputedNeighborhoodSearch
+        smoothing_length = neighborhood_search.neighborhood_search.search_radius
+    else
+        smoothing_length = neighborhood_search.search_radius
+    end
+    smoothing_kernel = WendlandC2Kernel{ndims(neighborhood_search)}()
+
+    solid_system = TotalLagrangianSPHSystem(solid, smoothing_kernel, smoothing_length,
+                                            material.E, material.nu)
+
+    v = copy(solid.velocity)
+    u = copy(solid.coordinates)
+    dv = zero(v)
+
+    return @belapsed TrixiParticles.interact!($dv, $v, $u, $v, $u, $neighborhood_search,
+                                              $solid_system, $solid_system)
 end
