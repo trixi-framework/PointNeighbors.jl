591827bdd986630f802642ce4f11a459588e0e67
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-20 10:37:57 +0200

    Implement neighborhood search based on static neighbor lists (#9)
    * Implement NHS based on static neighbor lists
    
    * Reformat code
    
    * Export `NeighborListsNeighborhoodSearch`
    
    * Add `NeighborListsNeighborhoodSearch` to tests
    
    * Rename variable
    
    * Fix `foreach_neighbor`
    
    * Improve comments
    
    * Add docs for `NeighborListsNeighborhoodSearch`
    
    * Add neighbor lists NHS to tests
    
    * Rename neighborhood search
    
    * Reformat

--- src/PointNeighbors.jl
+++ src/PointNeighbors.jl
@@ -9,11 +9,12 @@ using Polyester: @batch
 include("util.jl")
 include("neighborhood_search.jl")
 include("nhs_trivial.jl")
-include("nhs_grid.jl")
 include("cell_lists/cell_lists.jl")
+include("nhs_grid.jl")
+include("nhs_neighbor_lists.jl")
 
 export for_particle_neighbor, foreach_neighbor
-export TrivialNeighborhoodSearch, GridNeighborhoodSearch
+export TrivialNeighborhoodSearch, GridNeighborhoodSearch, PrecomputedNeighborhoodSearch
 export initialize!, update!, initialize_grid!, update_grid!
 
 end # module PointNeighbors
