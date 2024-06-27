30dcab81c171b2109258b1c60dbd8bdccb7c094a
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2024-06-27 17:09:27 +0200

    Reformat

--- src/gpu.jl
+++ src/gpu.jl
@@ -23,7 +23,10 @@ function Adapt.adapt_structure(to::typeof(Array), range::UnitRange)
     return range
 end
 
-function Adapt.adapt_structure(to, nhs::GridNeighborhoodSearch{<:Any, update_strategy}) where {update_strategy}
+function Adapt.adapt_structure(to,
+                               nhs::GridNeighborhoodSearch{<:Any, update_strategy}) where {
+                                                                                           update_strategy
+                                                                                           }
     (; search_radius, periodic_box, n_cells, cell_size) = nhs
 
     cell_list = Adapt.adapt_structure(to, nhs.cell_list)
