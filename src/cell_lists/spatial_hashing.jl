4b57a71dab9dc6ff8b1b2e4d95469f2be02ffa61
RubberLanding <nico.schumann@tutanota.com>
2025-02-27 17:03:38 +0800

    Spatial Hashing works with initialize!() and empty!().

--- src/cell_lists/spatial_hashing.jl
+++ src/cell_lists/spatial_hashing.jl
@@ -1,8 +1,8 @@
 struct SpatialHashingCellList{CL, CI, CF}
-    cell_points       :: CL
-    cell_coords :: CI
-    cell_collision :: CF
-    size        :: Int
+    size::Int
+    cell_points::CL
+    cell_coords::CI
+    cell_collision::CF
 end
 
 @inline index_type(::SpatialHashingCellList) = Int64
@@ -23,7 +23,6 @@ function Base.empty!(cell_list::SpatialHashingCellList)
     Base.empty!.(cell_list.cell_points)
     Base.empty!.(cell_list.cell_coords)
     cell_list.cell_collision .= false
-
     return cell_list
 end
 
@@ -31,33 +30,12 @@ function push_cell!(cell_list::SpatialHashingCellList, cell, point)
     (; cell_points, cell_coords, cell_collision, size) = cell_list
     key = spatial_hash(cell, size)
     push!(cell_points[key], point)
-
     if !(cell in cell_coords[key])
         push!(cell_coords[key], cell)
     end
-
     if length(cell_coords[key]) > 1
         cell_collision[key] = true
     end
-end
-
-function get_points(nhs, cell, coords_fun)
-    (; cell_list) = nhs
-    (; cell_points, cell_coords, cell_collision, size) = cell_list
-    key = spatial_hash(cell, size)
-    if cell_collision[key]
-        points_in_cell = []
-        for point in cell_list.cell_points[key]
-            if cell_coords(coords_fun(point), nhs) == cell
-                push!(points_in_cell, point)
-            end
-        end
-        return points_in_cell
-
-    else
-        return cell_list.cell_points[key]
-    end
-
 end
 
 function deleteat_cell!(cell_list::SpatialHashingCellList, cell, i)
@@ -66,17 +44,19 @@ end
 
 @inline each_cell_index(cell_list::SpatialHashingCellList) = eachindex(cell_list.cell_points)
 
-@inline function Base.getindex(cell_list::SpatialHashingCellList, i)
-    if isa(i, Int)
-        return cell_list.cell_points[i]
-    elseif isa(i, Tuple)
-        return cell_list.cell_points[i...]
-    end
+@inline function Base.getindex(cell_list::SpatialHashingCellList, cell::Tuple)
+    (; cell_points) = cell_list
+
+    return cell_points[spatial_hash(cell, length(cell_points))]
+end
+
+@inline function Base.getindex(cell_list::SpatialHashingCellList, i::Integer)
+    return cell_list.cell_points[i]
 end
 
 @inline function is_correct_cell(cell_list::SpatialHashingCellList{<:Any, Nothing},
-    cell_coords, cell_index::Array) # What is the correct type for cell_index? It is a list of integers
-return cell_coords in cell_list.cell_coords[cell_index]
+                                 cell_coords, cell_index::Array) # What is the correct type for cell_index? It is a list of integers
+    return cell_coords in cell_list.cell_coords[cell_index]
 end
 
 function spatial_hash(cell::NTuple{1, Real}, size)
@@ -100,3 +80,21 @@ function spatial_hash(cell::NTuple{3, Real}, index, size)
 
     return mod(xor(i * 73856093, j * 19349663, k * 83492791, index * 7238423947), size) + 1
 end
+
+# function get_points(nhs, cell, coords_fun)
+#     (; cell_list) = nhs
+#     (; cell_points, cell_coords, cell_collision, size) = cell_list
+#     key = spatial_hash(cell, size)
+#     if cell_collision[key]
+#         points_in_cell = []
+#         for point in cell_list.cell_points[key]
+#             if cell_coords(coords_fun(point), nhs) == cell
+#                 push!(points_in_cell, point)
+#             end
+#         end
+#         return points_in_cell
+
+#     else
+#         return cell_list.cell_points[key]
+#     end
+# end
--- test/nhs_spatial_hash.jl
+++ test/nhs_spatial_hash.jl
@@ -12,18 +12,17 @@
 
         # Create neighborhood search
         nhs1 = GridNeighborhoodSearch{2}(; search_radius, n_points,
-                                         cell_list = SpatialHashingCellList{2}(n_points))
+                                         cell_list = SpatialHashingCellList{2}(2 * n_points))
 
         initialize_grid!(nhs1, coordinates1)
 
         # Get each neighbor for `point_position1`
         neighbors1 = sort(collect(PointNeighbors.eachneighbor(point_position1, nhs1)))
 
-        # Move points
+        # Move points and update NHS
         coordinates2 = coordinates1 .+ [1.4, -3.5]
-
-        # Update neighborhood search
-        update_grid!(nhs1, coordinates2)
+        empty!(nhs1.cell_list)
+        initialize_grid!(nhs1, coordinates2)
 
         # Get each neighbor for updated NHS
         neighbors2 = sort(collect(PointNeighbors.eachneighbor(point_position1, nhs1)))
@@ -36,7 +35,9 @@
 
         # Double search radius
         nhs2 = GridNeighborhoodSearch{2}(search_radius = 2 * search_radius,
-                                         n_points = size(coordinates1, 2))
+                                         n_points = size(coordinates1, 2),
+                                         cell_list = SpatialHashingCellList{2}(2 * n_points))
+
         initialize!(nhs2, coordinates1, coordinates1)
 
         # Get each neighbor in double search radius
@@ -44,24 +45,28 @@
 
         # Move points
         coordinates2 = coordinates1 .+ [0.4, -0.4]
-
-        # Update neighborhood search
-        update!(nhs2, coordinates2, coordinates2)
+        empty!(nhs2.cell_list)
+        initialize_grid!(nhs2, coordinates2)
 
         # Get each neighbor in double search radius
         neighbors5 = sort(collect(PointNeighbors.eachneighbor(point_position1, nhs2)))
 
         #### Verification against lists of potential neighbors built by hand
-        @test neighbors1 == [17, 18, 19, 24, 25, 26, 31, 32, 33]
+        correct_neighbors1 = [17, 18, 19, 24, 25, 26, 31, 32, 33]
+        @test all(x -> x in neighbors1, correct_neighbors1)
 
-        @test neighbors2 == Int[]
+        correct_neighbors2 = Int[]
+        @test all(x -> x in neighbors2, correct_neighbors2)
 
-        @test neighbors3 == [17, 18, 19, 24, 25, 26, 31, 32, 33]
+        correct_neighbors3 = [17, 18, 19, 24, 25, 26, 31, 32, 33]
+        @test all(x -> x in neighbors3, correct_neighbors3)
 
-        @test neighbors4 == [
+        correct_neighbors4 = [
             9, 10, 11, 12, 13, 14, 16, 17, 18, 19, 20, 21, 23, 24, 25, 26, 27, 28, 30, 31,
             32, 33, 34, 35, 37, 38, 39, 40, 41, 42, 44, 45, 46, 47, 48, 49]
+        @test all(x -> x in neighbors4, correct_neighbors4)
 
-        @test neighbors5 == [36, 37, 38, 43, 44, 45]
+        correct_neighbors5 = [36, 37, 38, 43, 44, 45]
+        @test all(x -> x in neighbors5, correct_neighbors5)
     end
 end;
