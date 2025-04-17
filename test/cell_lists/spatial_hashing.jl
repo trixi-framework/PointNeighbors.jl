4b96172c52438e723749bb1697c99769a2c71021
RubberLanding <nico.schumann@tutanota.com>
2025-04-17 15:31:40 +0800

    Update test for spatial hashing, add test for empty list.

--- test/cell_lists/spatial_hashing.jl
+++ test/cell_lists/spatial_hashing.jl
@@ -2,10 +2,10 @@
     # General test for 2D and 3D
     @testset verbose=true "Compare Against `TrivialNeighborhoodSearch`" begin
         cloud_sizes = [
-            # (10, 11),
-            # (100, 90),
+            (10, 11),
+            (100, 90),
             (8, 10, 6),
-            # (39, 40, 41)
+            (39, 40, 41)
         ]
 
         name(size) = "$(length(size))D with $(prod(size)) Particles"
@@ -56,42 +56,27 @@
         end
     end
 
-    # # Explicit test, how the data structure handels collisions (very simple)
-    # @testset "Collisions with foreach_neighbor" begin
-    #     range = -0.35:0.1:0.25
-    #     coordinates1 = hcat(collect.(Iterators.product(range, range))...)
-    #     n_points = size(coordinates1, 2)
-    #     search_radius = 0.1 + 10 * eps()
-    #     point_index1 = 25
-    #     point_position1 = coordinates1[point_index1]
+    # Test list behavior with empty cells
+    @testset "Collision Handling with empty cells" begin
+        # The point is in cell (-1, 0) which has a hash collision with cell (-2, -1)
+        point = [-0.05, 0.05]
+        coordinates = hcat(point)
+        NDIMS = size(coordinates, 1)
+        n_points = size(coordinates, 2)
+        search_radius = 0.1 + 10 * eps()
+        point_index = 1
 
-    #     @testset verbose=true "List Size $(list_size)" for list_size in [
-    #         2 * n_points,
-    #         1,
-    #         n_points,
-    #         4 * n_points
-    #     ]
-    #         nhs1 = GridNeighborhoodSearch{2}(; search_radius, n_points,
-    #                                          cell_list = SpatialHashingCellList{2}(list_size))
-    #         initialize_grid!(nhs1, coordinates1)
+        nhs = GridNeighborhoodSearch{2}(; search_radius, n_points,
+                                            cell_list = SpatialHashingCellList{NDIMS}(n_points))
+        initialize_grid!(nhs, coordinates)
 
-    #         @testset verbose=true "Collision at test point" begin
-    #             cell = PointNeighbors.cell_coords(point_position1, nhs1)
-    #             index = PointNeighbors.spatial_hash(cell, nhs1.cell_list.list_size)
-    #             @test nhs1.cell_list.collisions[index] == true
-    #         end
+        found_neighbors = Int[]
+        foreach_neighbor(coordinates, coordinates, nhs,
+                            point_index) do point, neighbor, pos_diff, distance
+            push!(found_neighbors, neighbor)
+        end
 
-    #         found_neighbors1 = Int[]
-    #         foreach_neighbor(coordinates1, coordinates1, nhs1,
-    #                          point_index1) do point, neighbor, pos_diff, distance
-    #             push!(found_neighbors1, neighbor)
-    #         end
-
-    #         correct_neighbors1 = [18, 24, 25, 26, 32]
-    #         @test correct_neighbors1 == found_neighbors1
-    #     end
-    # end
-    # @testset "Collision Handling with empty cells" begin
-    #     #TODO: Add explicit test for list behavior with empty cells
-    # end
+        correct_neighbors = [1]
+        @test correct_neighbors == found_neighbors
+    end
 end
