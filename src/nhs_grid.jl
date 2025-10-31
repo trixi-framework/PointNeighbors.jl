4d7774bdc4a0a36252911f336f8c5b31ebe44654
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2025-10-31 18:32:58 +0100

    Simplify

--- src/nhs_grid.jl
+++ src/nhs_grid.jl
@@ -504,7 +504,6 @@ end
                                   point, point_coords, search_radius)
     (; cell_list, periodic_box) = neighborhood_search
     cell = cell_coords(point_coords, neighborhood_search)
-    ELTYPE = eltype(neighborhood_search)
 
     for neighbor_cell_ in neighboring_cells(cell, neighborhood_search)
         neighbor_cell = Tuple(neighbor_cell_)
@@ -522,7 +521,7 @@ end
             neighbor_coords = extract_svector(neighbor_system_coords,
                                               Val(ndims(neighborhood_search)), neighbor)
 
-            pos_diff = convert.(ELTYPE, point_coords - neighbor_coords)
+            pos_diff = convert.(eltype(neighborhood_search), point_coords - neighbor_coords)
             distance2 = dot(pos_diff, pos_diff)
 
             pos_diff,
