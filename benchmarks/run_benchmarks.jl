9bce3baab5f5c16a938a43d9cddc2b1e122a7063
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-04-09 10:44:49 +0200

    Pre-sort benchmark and add `run_benchmark_full_grid`

--- benchmarks/run_benchmarks.jl
+++ benchmarks/run_benchmarks.jl
@@ -65,7 +65,8 @@ function run_benchmark(benchmark, n_points_per_dimension, iterations, neighborho
     times = zeros(iterations, length(neighborhood_searches))
 
     for iter in 1:iterations
-        coordinates_ = point_cloud(sizes[iter]; seed, perturbation_factor_position, shuffle)
+        coordinates_ = point_cloud(sizes[iter], search_radius_factor;
+                                   seed, perturbation_factor_position, shuffle)
         coordinates = convert.(typeof(search_radius_factor), coordinates_)
         domain_size = maximum(sizes[iter]) + 1
 
@@ -189,13 +190,57 @@ function run_benchmark_gpu(benchmark, n_points_per_dimension, iterations;
         grid_nhs
         PrecomputedNeighborhoodSearch{NDIMS}(; search_radius = 0.0f0,
                                              update_neighborhood_search = grid_nhs,
-                                             transpose_backend)
+                                             transpose_backend)#, max_neighbors=128)
     ]
 
     names = [
         "GridNeighborhoodSearch with FullGridCellList";;
         "PrecomputedNeighborhoodSearch"
     ]
+
+    run_benchmark(benchmark, n_points_per_dimension, iterations,
+                  neighborhood_searches; names, parallelization_backend, kwargs...)
+end
+
+"""
+    run_benchmark_full_grid(benchmark, n_points_per_dimension, iterations; kwargs...)
+
+Shortcut to call [`run_benchmark`](@ref) with a `GridNeighborhoodSearch` with a
+`FullGridCellList`. This is the neighborhood search implementation that is used
+in TrixiParticles.jl when performance is important.
+Use this function to benchmark and profile TrixiParticles.jl kernels.
+
+# Arguments
+- `benchmark`:              The benchmark function. See [`benchmark_count_neighbors`](@ref),
+                            [`benchmark_n_body`](@ref), [`benchmark_wcsph`](@ref),
+                            [`benchmark_wcsph_fp32`](@ref) and [`benchmark_tlsph`](@ref).
+- `n_points_per_dimension`: Initial resolution as tuple. The product is the initial number
+                            of points. For example, use `(100, 100)` for a 2D benchmark or
+                            `(10, 10, 10)` for a 3D benchmark.
+- `iterations`:             Number of refinement iterations
+
+# Keywords
+See [`run_benchmark`](@ref) for a list of available keywords.
+
+# Examples
+```julia
+include("benchmarks/benchmarks.jl")
+
+run_benchmark_full_grid(benchmark_n_body, (10, 10), 3)
+```
+"""
+function run_benchmark_full_grid(benchmark, n_points_per_dimension, iterations;
+                           parallelization_backend=PolyesterBackend(), kwargs...)
+    NDIMS = length(n_points_per_dimension)
+
+    min_corner = 0.0f0 .* n_points_per_dimension
+    max_corner = Float32.(n_points_per_dimension ./ maximum(n_points_per_dimension))
+    cell_list = FullGridCellList(; search_radius = 0.0f0, min_corner, max_corner)
+    grid_nhs = GridNeighborhoodSearch{NDIMS}(; search_radius = 0.0f0, cell_list,
+                                             update_strategy = ParallelUpdate())
+    neighborhood_searches = [grid_nhs]
+
+    names = ["GridNeighborhoodSearch with FullGridCellList";;]
 
     run_benchmark(benchmark, n_points_per_dimension, iterations,
                   neighborhood_searches; names, parallelization_backend, kwargs...)
